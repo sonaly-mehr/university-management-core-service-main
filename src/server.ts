ff2c40c7c61c3aaefac055921856063bfc87b9ec
sonaly-mehr <sonaly.mehr@gmail.com>
2024-05-07 00:22:15 +0600

    updated

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,6 @@ import { Server } from 'http';
 import app from './app';
 import subscribeToEvents from './app/events';
 import config from './config';
-import { errorlogger, logger } from './shared/logger';
 import { RedisClient } from './shared/redis';
 
 
@@ -11,21 +10,21 @@ async function bootstrap() {
     subscribeToEvents()
   });
   const server: Server = app.listen(config.port, () => {
-    logger.info(`Server running on port ${config.port}`);
+    console.log(`Server running on port ${config.port}`);
   });
 
   const exitHandler = () => {
 
     if (server) {
       server.close(() => {
-        logger.info('Server closed');
+        console.log('Server closed');
       });
     }
     process.exit(1);
   };
 
   const unexpectedErrorHandler = (error: unknown) => {
-    errorlogger.error(error);
+    console.log(error);
     exitHandler();
   };
 
@@ -33,7 +32,7 @@ async function bootstrap() {
   process.on('unhandledRejection', unexpectedErrorHandler);
 
   // process.on('SIGTERM', () => {
-  //   logger.info('SIGTERM received');
+  //   console.log('SIGTERM received');
   //   if (server) {
   //     server.close();
   //   }
